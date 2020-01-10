0006da50f4a7545ad9ab77a4d019bdcfa2471918
gmer <guerric.merle@hcuge.ch>
2020-01-10 21:46:53 +0100

    first commit

---
+++ src/test/java/ch/hcuge/springcloud/MsHoxtonApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hcuge.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHoxtonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
