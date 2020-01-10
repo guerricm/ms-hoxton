45a2e29e97dea05d4ad32a5b768a2001229b6e9d
gmer <guerric.merle@hcuge.ch>
2020-01-10 21:58:47 +0100

    add rest controller

--- src/main/java/ch/hcuge/springcloud/MsHoxtonApplication.java
+++ src/main/java/ch/hcuge/springcloud/MsHoxtonApplication.java
@@ -3,6 +3,9 @@ package ch.hcuge.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,17 @@ public class MsHoxtonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsHoxtonApplication.class, args);
+	}
+	
+	@RestController
+	@RequestMapping
+	class ControllerTest {
+		
+		@GetMapping("/hello")
+		public String test() {
+			return "Hello World";
+		}
+		
 	}
 
 }
